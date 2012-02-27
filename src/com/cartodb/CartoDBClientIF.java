414edf58662d16360948a9bb9a76bd0b5b7b70d4
cgendreau <christiangendreau@gmail.com>
2012-02-27 09:50:00 -0500

    CartoDBResponse class and better README

--- src/com/cartodb/CartoDBClientIF.java
+++ src/com/cartodb/CartoDBClientIF.java
@@ -1,11 +1,16 @@
 package com.cartodb;
 
+/**
+ * Interface for CartoDB client implementation
+ * @author canadensys
+ *
+ */
 public interface CartoDBClientIF {
 	
 	/**
-	 * This executes a query on the CartoDB server
-	 * @param sqlQuery
-	 * @return response as JSON string
+	 * Executes a query on the CartoDB server
+	 * @param sqlQuery do not URL encode the query, this function will do it.
+	 * @return response as JSON string or null if the query cannot be completed
 	 */
 	public String executeQuery(String sqlQuery);
 
